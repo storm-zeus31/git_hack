9c96b9d95c53bf036d5607d7533d0807e4fef708
storm-zeus31 <soumya.placement.info@gmail.com>
2022-09-16 12:04:25 +0530

    changed classifyImage method inside post call

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from fastapi import FastAPI,File,UploadFile
-from flask import jsonify
-import tensorflow as tf
 import uvicorn
 from fastapi.responses import Response
-import uuid
 from starlette.responses import RedirectResponse
-
-
-
 from classification import prediction, read_imagefile
+
+val= []
 
 app =FastAPI(title='Image Classifier')
 
@@ -16,7 +12,7 @@ app =FastAPI(title='Image Classifier')
 async def classifyImage(file: UploadFile = File(...)):
     extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
     if not extension:
-        return "Image must be jpg or png format!"
+        return "Image should be jpg or png format!"
     image = read_imagefile(await file.read())
     predictionResult = prediction(image)
 
